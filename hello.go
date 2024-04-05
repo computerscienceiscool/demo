dce86921c0ff303fba17e86d757ae73038a501df
computerscienceiscool <jjsalley@gmail.com>
2024-04-05 01:31:50 -0700

    Adding files

---
+++ hello.go
@@ -0,0 +1,17 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Println("Hello, World! 1")
+   fmt.Println("Hello\t World! 2")
+    
+   fmt.Println("Hello\n World! 3")
+    
+   fmt.Println("Hello" + "World! 4")
+    
+
+
+
+}
+
